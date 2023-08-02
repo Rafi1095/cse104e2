eeb0c4618fab3f878b7f908b2afdca1adc0d4be2
Rafi1095 <141204390+Rafi1095@users.noreply.github.com>
2023-08-02 10:26:53 +0600

    Add files via upload

---
+++ problem4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    float r=2,pi=3.1416,area,volume;
+
+    area=pi*r*r;
+    volume=(4/3)*pi*r*r*r;
+    printf("%.2f\n",area);
+    printf("%.2f\n",volume);
+
+
+
+
+}
